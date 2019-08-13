2c638812a46ce79715ddbb8ba74b0b81c4619f34
David de Jong <daviddejong@me.com>
2019-08-13 17:19:21 +0200

    fixed image read

--- utils/frame_utils.py
+++ utils/frame_utils.py
@@ -1,7 +1,7 @@
 import numpy as np
 from os.path import *
-# from scipy.misc import imread
-from skimage import io
+from scipy.misc import imread
+# from skimage import io
 from . import flow_utils 
 
 def read_gen(file_name,as_grayscale=False):
@@ -9,9 +9,9 @@ def read_gen(file_name,as_grayscale=False):
     if ext == '.png' or ext == '.jpeg' or ext == '.ppm' or ext == '.jpg':
         if as_grayscale:
             print('converted to grayscale')
-            im = io.imread(file_name,as_grayscale=True)
+            im = imread(file_name,as_grayscale=True)
         else:
-            im = io.imread(file_name)
+            im = imread(file_name)
         if im.shape[2] > 3:
             return im[:,:,:3]
         else:
